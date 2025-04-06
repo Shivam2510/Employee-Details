cd004006e50120952d93e5d34c676f3ebccdcf18
Shivam2510 <shivammandloi25@gmail.com>
2025-04-06 21:14:17 +0530

    Employee details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Shared/Components/header/header.component';
+import { EmployeeDetailsAddFormComponent } from './Employee/employee-details-add-form/employee-details-add-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, EmployeeDetailsAddFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
+import { EmployeeDetailsAddFormComponent } from './Employee/employee-details-add-form/employee-details-add-form.component';
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./Employee/employee-list/employee-list.component').then(
+          (m) => m.EmployeeListComponent
+        ),
+    },
+    {
+      path: 'employee-details',
+      loadComponent: () =>
+        import('./Employee/employee-details-add-form/employee-details-add-form.component').then(
+          (m) => m.EmployeeDetailsAddFormComponent
+        ),
+    },
+    {
+      path: '**',
+      redirectTo: '',
+      pathMatch: 'full',
+    },
+  ];
